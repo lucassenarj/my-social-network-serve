4d5d0c6119736d4c6ba394059abb3868656e9a93
Lucas Sena <lucassena.rj@gmail.com>
2018-09-11 00:05:50 -0300

    Reponse like with status

--- app/Controllers/Http/LikeController.js
+++ app/Controllers/Http/LikeController.js
@@ -4,16 +4,15 @@ const Post = use('App/Models/Post')
 
 class LikeController {/**
   * * Create/save a new like
-  * POST likes
-  */
- async store ({ params, auth }) {
+   * POST likes
+   */
+  async store ({ params, auth, response }) {
+    const post = await Post.findOrFail(params.id)
 
-  const post = await Post.findOrFail(params.id)
+    await post.likes().create({user_id: auth.user.id, like: 1})
 
-  await post.likes().create({user_id: auth.user.id, like: 1})
-
-  return post
- }
+    return response.status(200).send({message: 'Post liked!'})
+  }
 }
 
 module.exports = LikeController
