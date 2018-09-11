9a54edfa168fe1ab9b01f28258e535aeaea20b92
Lucas Sena <lucassena.rj@gmail.com>
2018-09-10 22:26:08 -0300

    Implementing signup and login

--- database/migrations/1503250034279_user.js
+++ database/migrations/1503250034279_user.js
@@ -8,6 +8,7 @@ class UserSchema extends Schema {
       table.increments()
       table.string('username', 80).notNullable().unique()
       table.string('email', 254).notNullable().unique()
+      table.string('name', 80).notNullable()
       table.string('password', 60).notNullable()
       table.timestamps()
     })
